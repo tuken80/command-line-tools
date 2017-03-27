124ee02a4ab06c3fa5f01103d83f6b51b57e825f
Romain Duquesne <romain.duquesne.mail@gmail.com>
2017-03-27 22:25:02 +0200

    Initial Commit

---
+++ SearchStringIn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchStringIn
+//
+//  Created by Romain Duquesne on 27/03/2017.
+//  Copyright © 2017 Romain Duquesne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
